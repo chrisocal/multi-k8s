c7a673311c4f9f709e41113e648c093c7e32f6b4
CHRISTOPHER OMAR PANGANIBAN CALUAG <chriscal@sg.ibm.com>
2021-05-10 16:16:43 +0800

    init commit.

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+docker build -t chrisocal/multi-client:latest -t chrisocal/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t chrisocal/multi-server:latest -t chrisocal/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t chrisocal/multi-worker:latest -t chrisocal/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker push chrisocal/multi-client:latest
+docker push chrisocal/multi-server:latest
+docker push chrisocal/multi-worker:latest
+docker push chrisocal/multi-client:$SHA
+docker push chrisocal/multi-server:$SHA
+docker push chrisocal/multi-worker:$SHA
+kubectl apply -f k8s
+kubectl set image deployments/server-deployment server=chrisocal/multi-server:$SHA
+kubectl set image deployments/client-deployment server=chrisocal/multi-client:$SHA 
+kubectl set image deployments/worker-deployment server=chrisocal/multi-worker:$SHA
