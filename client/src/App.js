f0bf652460d0ed52546b044d953d769071303de1
CHRISTOPHER OMAR PANGANIBAN CALUAG <chriscal@sg.ibm.com>
2021-05-10 17:53:41 +0800

    added labels.

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Kubernetes Deployment by Chris Caluag
+            K8s App by Chris Caluag
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
